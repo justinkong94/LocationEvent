046a756164a01438678fe6b38bd261d9f13a175a
justinkong94 <justinkong24@yahoo.com>
2016-11-17 18:09:52 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = null;
+
+        try{
+            System.out.println("Please input coordinates:");
+            sc = new Scanner(System.in);
+            int xCoordinate = sc.nextInt();
+        }catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
