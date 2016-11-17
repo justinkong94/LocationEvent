5c429cc5723ff35483587ab43248d2fcd6179d01
justinkong94 <justinkong24@yahoo.com>
2016-11-17 18:57:30 +0000

    Added input function, tested for invalid inputs. Only allows valid coordinate input.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,39 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner sc = null;
+        final int COORDINATEDIMENSION = 2;
 
-        try{
-            System.out.println("Please input coordinates:");
-            sc = new Scanner(System.in);
-            int xCoordinate = sc.nextInt();
-        }catch (Exception e) {
-            e.printStackTrace(System.err);
-            System.out.println("Invalid input");
+        Scanner sc;
+        Boolean isRunning = true;
+        String input;
+        String[] inputSplit;
+        int xCoordinate = 0;
+        int yCoordinate = 0;
+
+        while(isRunning) {
+            try {
+                System.out.println("Please input coordinates:");
+                System.out.printf(">");
+                sc = new Scanner(System.in);
+                input = sc.next();
+                inputSplit = input.split(",");
+
+                if(inputSplit.length != COORDINATEDIMENSION){
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+
+                xCoordinate = Integer.parseInt(inputSplit[0]);
+                yCoordinate = Integer.parseInt(inputSplit[1]);
+
+            } catch(ArrayIndexOutOfBoundsException e){
+                //e.printStackTrace(System.err);
+                System.out.println("Not a coordinate.");
+            } catch(NumberFormatException e){
+                //e.printStackTrace(System.err);
+                System.out.println("Invalid input.");
+            } catch(Exception e){
+                e.printStackTrace(System.err);
+            }
         }
     }
 }
