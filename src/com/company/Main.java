71091d4ee731e0c4f25dcd0c0536f36e5b3b967c
justinkong94 <justinkong24@yahoo.com>
2016-11-18 18:39:24 +0000

    Fixed event not found on selected coordinate.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,10 @@ public class Main {
 
     private final static int COORDINATEDIMENSION = 2;
     private final static int NUMBEROFCLOSESTEVENTS = 5;
-    private final static int XAXISMIN = -5;
-    private final static int XAXISMAX = 7;
-    private final static int YAXISMIN = -3;
-    private final static int YAXISMAX = 6;
+    private final static int XAXISMIN = -10;
+    private final static int XAXISMAX = 10;
+    private final static int YAXISMIN = -10;
+    private final static int YAXISMAX = 10;
     //assume max number of tickets for each event is 20
     private final static int MAXNUMBEROFTICKETS = 20;
     //assume max price of each ticket is 50 dollars
@@ -26,6 +26,7 @@ public class Main {
 
         GenerateEvents(world,xAxisRange,yAxisRange);
 
+        //assume in the case that a few events have the same distance and causes list to exceed 5, events will be picked arbitrarily
         //assume events with no tickets are still shown
         //assume coordinates are linear in distance
         //assume there is at least 1 event
@@ -37,10 +38,11 @@ public class Main {
         //make sure ticket prices are all 2 decimals
         //make sure event number are all 3 digits
 
+        //distance not in order (maybe done)
         //no repeats of search nodes (maybe done)
         //no prints of more than 5 nearby events
-        //check for events on same spot as user
-        //arraylist listOfNodes out of bounds error sometimes
+        //check for events on same spot as user(maybe done)
+        //arraylist listOfNodes out of bounds error sometimes(maybe done)
         //isRunning never quits
 
 
@@ -50,7 +52,7 @@ public class Main {
                 System.out.printf(">");
                 sc = new Scanner(System.in);
                 String input = sc.next();
-                String[] inputSplit = input.split(",");
+                String[] inputSplit = input.split(",",2);
 
                 if(inputSplit.length != COORDINATEDIMENSION){
                     throw new InputException("Not a coordinate.");
@@ -66,12 +68,14 @@ public class Main {
                     throw new InputException("Invalid coordinates.");
                 }
 
+                System.out.println("Closest Events to (" + inputXCoordinate + "," + inputYCoordinate + "):");
                 int numberOfFoundEvents = 0;
                 ArrayList<Node> listOfNodes = new ArrayList<Node>();
+
                 listOfNodes.add(new Node(inputXCoordinate - XAXISMIN,inputYCoordinate - YAXISMIN));
+                numberOfFoundEvents += CheckCoordinateDetails(world.GetCoordinates(inputXCoordinate - XAXISMIN,inputYCoordinate - YAXISMIN),world,inputXCoordinate,inputYCoordinate);
                 world.SetSearchedIndex(inputXCoordinate - XAXISMIN,inputYCoordinate - YAXISMIN);
-                System.out.println("Closest Events to (" + inputXCoordinate + "," + inputYCoordinate + "):");
-                //SearchNearestEvents(inputXCoordinate,inputYCoordinate,xAxisRange,yAxisRange,world,listOfEvents,NUMBEROFCLOSESTEVENTS);
+
                 while(numberOfFoundEvents < NUMBEROFCLOSESTEVENTS){
                     int currentListSize = listOfNodes.size();
                     for(int i=0;i<currentListSize;i++){
@@ -119,73 +123,6 @@ public class Main {
         }
     }
 
-    private static void SearchNearestEvents(int inputXCoordinate, int inputYCoordinate, int xAxisRange, int yAxisRange, int[][] world, ArrayList<Event> listOfEvents, int numOfEventsToSearch){
-        int currentEventsFound = 0;
-        ArrayList<Event> listOfClosestEvents = new ArrayList<Event>();
-        //set all currentScan coordinates to array index which represents world.
-        int currentScanMinXCoord = inputXCoordinate - XAXISMIN;
-        int currentScanMaxXCoord = inputXCoordinate - XAXISMIN;
-        int currentScanMinYCoord = inputYCoordinate - YAXISMIN;
-        int currentScanMaxYCoord = inputYCoordinate - YAXISMIN;
-        Boolean isSearchComplete = false;
-        int[][] visitedCoordinates = new int[xAxisRange][yAxisRange];
-
-        while(currentEventsFound < numOfEventsToSearch){
-            if(currentScanMinXCoord > 0){
-                currentScanMinXCoord--;
-                isSearchComplete = false;
-            }
-            if(currentScanMinYCoord > 0){
-                currentScanMinYCoord--;
-                isSearchComplete = false;
-            }
-            if(currentScanMaxXCoord < xAxisRange - 2){
-                currentScanMaxXCoord++;
-                isSearchComplete = false;
-            }
-            if(currentScanMaxYCoord < yAxisRange - 2){
-                currentScanMaxYCoord++;
-                isSearchComplete = false;
-            }
-
-            if(isSearchComplete){
-                break;
-            }
-
-            for(int i=currentScanMinXCoord;i<=currentScanMaxXCoord;i++){
-                if(world[i][currentScanMaxYCoord] > 0 && visitedCoordinates[i][currentScanMaxYCoord] == 0){
-                    listOfClosestEvents.add(listOfEvents.get(world[i][currentScanMaxYCoord]));
-                    currentEventsFound++;
-                }
-                visitedCoordinates[i][currentScanMaxYCoord] = 1;
-
-                if(world[i][currentScanMinYCoord] > 0 && visitedCoordinates[i][currentScanMinYCoord] == 0){
-                    listOfClosestEvents.add(listOfEvents.get(world[i][currentScanMinYCoord]));
-                    currentEventsFound++;
-                }
-                visitedCoordinates[i][currentScanMinYCoord] = 1;
-            }
-            for(int i=currentScanMinYCoord+1;i<currentScanMaxYCoord;i++){
-                if(world[currentScanMinXCoord][i] > 0 && visitedCoordinates[currentScanMinXCoord][i] == 0){
-                    listOfClosestEvents.add(listOfEvents.get(world[currentScanMinXCoord][i]));
-                    currentEventsFound++;
-                }
-                visitedCoordinates[currentScanMinXCoord][i] = 1;
-
-                if(world[currentScanMaxXCoord][i] > 0 && visitedCoordinates[currentScanMaxXCoord][i] == 0){
-                    listOfClosestEvents.add(listOfEvents.get(world[currentScanMaxXCoord][i]));
-                    currentEventsFound++;
-                }
-                visitedCoordinates[currentScanMaxXCoord][i] = 1;
-            }
-
-            isSearchComplete = true;
-        }
-
-        SortClosestEvents(inputXCoordinate,inputYCoordinate,listOfClosestEvents);
-
-    }
-
     private static int SearchNeighbours(World world, ArrayList<Node> listOfNodes, int inputXCoord, int inputYCoord, int currentXIndex, int currentYIndex, int xAxisRange, int yAxisRange){
         int coordinateDetails;
         int numberOfSearchedEvents = 0;
@@ -226,37 +163,6 @@ public class Main {
         }
 
         return foundEvent;
-    }
-
-    private static void SortClosestEvents(int inputXCoordinate, int inputYCoordinate, ArrayList<Event> listOfClosestEvents){
-        ArrayList<Integer> distanceOfEvents = new ArrayList<Integer>();
-
-        for (Event listOfClosestEvent : listOfClosestEvents) {
-            distanceOfEvents.add(listOfClosestEvent.getDistance(inputXCoordinate, inputYCoordinate));
-        }
-
-        for(int i=0;i<listOfClosestEvents.size();i++) {
-
-            System.out.printf("Event %d : Distance %d\n",listOfClosestEvents.get(i).getEventNumber(),distanceOfEvents.get(i));
-        }
-        /*
-        for(int i=0;i<NUMBEROFCLOSESTEVENTS;i++){
-            int minimumDistanceIndex = 0;
-            int minimumDistance = distanceOfEvents.get(0);
-
-            for(int j=1;j<distanceOfEvents.size();j++){
-                if(distanceOfEvents.get(j) < minimumDistance){
-                    minimumDistanceIndex = j;
-                    minimumDistance = distanceOfEvents.get(j);
-                }
-            }
-
-            //printEventDetails(listOfClosestEvents.get(minimumDistanceIndex),minimumDistance);
-
-            distanceOfEvents.remove(minimumDistanceIndex);
-            listOfClosestEvents.remove(minimumDistanceIndex);
-        }*/
-
     }
 
     private static void printEventDetails(Event currentEvent, int inputXCoord, int inputYCoord){
